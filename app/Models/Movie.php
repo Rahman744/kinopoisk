3fb8719b33df9ec81649a6e75fa2e028014f2e47
Prog <prog64650@gmail.com>
2025-04-22 23:03:55 +0500

    second commit

---
+++ app/Models/Movie.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    use HasFactory;
+
+    public function genre()
+    {
+        return $this->belongsTo(Genre::class);
+    }
+
+    public function actors()
+    {
+        return $this->belongsToMany(Actor::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+}
